139c7fe1bcf8d5284d2e7d3d8d3caac03ab7c7bf
dev <80750539+sledge-dev@users.noreply.github.com>
2021-04-14 16:55:08 +0200

    updated testnet contracts

--- deploy/203_set_distribution.ts
+++ deploy/203_set_distribution.ts
@@ -7,24 +7,17 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, get, execute } = deployments
   const { deployer } = await getNamedAccounts()
   let gondolaUSDPoolLPToken = (await get("GondolaUSDPoolLPToken")).address
+  let GondolaToken = (await get("GondolaToken")).address
+  const now = 1617890000
 
-  const now = 1617450000
-
-  await execute(
-    "MasterChef",
-    { from: deployer, log: true },
-    "setRewards",
-    now + 1,
-    now + 3600 * 24 * 31 * 2,
-    BigNumber.from("18667861400000000000"),
-  )
+  await execute("MasterChef",{ from: deployer, log: true },"setRewards",now + 1,now + 3600 * 24 * 31 * 2,BigNumber.from("18667861400000000000"),)
   
   await execute(
     "MasterChef",
     { from: deployer, log: true },
     "set",
     0,
-    "0xe67F3fda5E24EdEbcADDFE9A4a222514013B6e21",
+    0,
     false,
   )
 
@@ -37,6 +30,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     false,
   )
 
+  await execute(
+    "MasterChef",
+    { from: deployer, log: true },
+    "add",
+    500,
+    GondolaToken,
+    false,
+  )
 }
 
 export default func
